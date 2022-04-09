a141f757c0e1df0b31344245151aa13638ff22b9
Ricardo Granja Chávez <ricardo.granja.chavez@gmail.com>
2022-04-09 13:21:45 -0500

    Configs

---
+++ examenios/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  examenios
+//
+//  Created by Ricardo Granja Chávez on 09/04/22.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    static var standard: AppDelegate { UIApplication.shared.delegate as! AppDelegate }
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // MARK: - Window Settings
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: MenuViewController())
+        window?.makeKeyAndVisible()
+        AppDelegate.standard.window = window
+        
+        return true
+    }
+
+}
+
