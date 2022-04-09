ea7aa8ea2d56bd62083b0afca1e351ccc8a77f15
Ricardo Granja Chávez <ricardo.granja.chavez@gmail.com>
2022-04-09 15:15:07 -0500

    image picker component

---
+++ examenios/View+Controllers/Components/MultimediaImagePickerController.swift
@@ -0,0 +1,31 @@
+//
+//  MultimediaImagePickerController.swift
+//  examenios
+//
+//  Created by Ricardo Granja Chávez on 09/04/22.
+//
+
+import Foundation
+
+import UIKit
+
+class MultimediaImagePickerController: UIImagePickerController {
+    var getOriginalImage: (UIImage) -> Void = { (_) in }
+    var getEditedImage: (UIImage) -> Void = { (_) in }
+}
+
+extension MultimediaImagePickerController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            getOriginalImage(image)
+        }
+        if let image = info[.editedImage] as? UIImage {
+            getEditedImage(image)
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
